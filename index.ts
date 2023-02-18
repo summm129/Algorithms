96ca63af7efe0ace780ff48e222058283c70ee9f
summm129 <97378571+summm129@users.noreply.github.com>
2023-02-19 00:52:57 +0900

    画面から引数渡せるようにアプデ

--- index.ts
+++ index.ts
@@ -1,14 +1,41 @@
-const array = [2,3,9,1,6,7,5];
-console.log(`今から[${array}]を昇順に整列させます。`);
+const result = document.getElementById('result');
+const sortButton = document.getElementById('sort');
+const clearButton = document.getElementById('clear');
 
-for(let j=0; j<array.length-1; j++){
-  for(let i=0; i<array.length-1; i++){
-    if(array[i]>array[i+1]){
-      const temp = array[i];
-      array[i]=array[i+1];
-      array[i+1]=temp;
+function sort(input: number[]) {
+  // const array = [2,3,9,1,6,7,5];
+  const array = input;
+  console.log(`今から[${array}]を昇順に整列させます。`);
+
+  console.log(`[${array}]`);
+  for (let j = 0; j < array.length - 1; j++) {
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        const temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        console.log(`[${array}]`);
+        result.innerText = result.innerText + `[${array}] \n`;
+      }
     }
   }
+
+  console.log(`バブルソート結果：[${array}]`);
 }
 
-console.log(`バブルソート：[${array}]`);
+sortButton.addEventListener('click', () => {
+  const input = <HTMLInputElement>document.getElementById('input');
+  if (input) {
+    const targetArray = input.value.split(',').map(Number);
+    sort(targetArray);
+  }
+});
+
+clearButton.addEventListener('click', () => {
+  result.innerText = '';
+});
+
+// function test(){
+//   console.log('testが呼ばれました')
+// }
+// window.test = test;
